f9b8749b16bd4d9c954620adf718e22bb6cd2334
Theusaopia <matheusdyas007@hotmail.com>
2022-10-04 09:33:46 -0300

    docker: iniciando conteinirizacao

--- licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -1,7 +1,6 @@
 package com.optimagrowth.license.service;
 
 import com.optimagrowth.license.model.License;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
